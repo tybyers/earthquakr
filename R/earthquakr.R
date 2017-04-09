8da7be7ab233a39173f192b20d9b5ea91456b840
Tyler Byers <tybyers@gmail.com>
2017-04-09 17:23:09 -0600

    initial commit for earthquakr.R and package documentation

---
+++ R/earthquakr.R
@@ -0,0 +1,11 @@
+#' earthquakr: A visualization package for world earthquake histories
+#'
+#' This package maps and timelines earthquakes.
+#'
+#' @importFrom dplyr filter mutate select "%>%"
+#' @importFrom purrr map map2_chr
+#' @importFrom stringr str_trim str_to_title
+#'
+#' @docType package
+#' @name earthquakr
+NULL
