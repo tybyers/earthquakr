7610a480f090fddd305c9f032d5e9c66846a0bae
Tyler Byers <tybyers@gmail.com>
2017-04-17 21:00:38 -0600

    import ggplot leaflet; suggests lubridate

--- R/earthquakr.R
+++ R/earthquakr.R
@@ -3,6 +3,8 @@
 #' This package maps and timelines earthquakes.
 #'
 #' @importFrom dplyr filter mutate select "%>%"
+#' @import ggplot2
+#' @import leaflet
 #' @importFrom purrr map map2_chr
 #' @importFrom stringr str_trim str_to_title
 #'
