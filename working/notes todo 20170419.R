932468b3ee3672b897ee2deb6556ca765cfafdbd
Tyler Byers <tybyers@gmail.com>
2017-04-20 21:12:04 -0600

    to-do notes 20170419

---
+++ working/notes todo 20170419.R
@@ -0,0 +1,10 @@
+#To do, as of 4/19/2017, before 4/23/2017:
+
+# geom_timeline_label doc and unit tests
+# eq_timeline documentation and unit tests
+#xx License file
+#xx DESCRIPTION file
+# Vignette
+# README
+
+## Next week: Get it to build!!  Standard evaluation checks
