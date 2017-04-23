1d7e886e2c258d30b8d7f082851388a834854076
Tyler Byers <tybyers@gmail.com>
2017-04-23 11:36:19 -0600

    working notes to self

--- working/notes todo 20170419.R
+++ working/notes todo 20170419.R
@@ -1,7 +1,8 @@
 #To do, as of 4/19/2017, before 4/23/2017:
 
-# geom_timeline_label doc and unit tests
+# geom_timeline_label unit tests
 # eq_timeline documentation and unit tests
+# redo documentation for the geoms with an "aesthetics" section
 #xx License file
 #xx DESCRIPTION file
 # Vignette
